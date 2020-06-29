1837b995687cd575aac84e7a6823938237f3cd01
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-06-29 11:41:33 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -17,7 +17,7 @@ class PaperBall{
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         rectMode(CENTER)
-        rect(0,0,this.width,this.height);  
+        circle(30,30,20);  
         pop();
       }
 }
