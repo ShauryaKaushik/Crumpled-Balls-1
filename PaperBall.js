02fb7a697a9e504f38ebdec39ad824848da2fa1f
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-02 20:01:47 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -4,7 +4,7 @@ class PaperBall{
             "isStatic" : true,
             'restitution':1.5,
             'friction':0.5,
-            'density':1.5
+            'density':1.5;
         }
         this.body = Bodies.circle(x, y,radius);
         this.width = width;
