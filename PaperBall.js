363e9e821f35b32e65371f2a856e8c56bbaa8146
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-02 19:59:56 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -16,7 +16,8 @@ class PaperBall{
         push();
         translate(this.body.position.x, this.body.position.y);
         ellipseMode(RADIUS);
-        fill("purple");
+        strokeWeight(5);  
+        fill("blue");
         ellipse(100,100,30,30);
         pop();
       }
