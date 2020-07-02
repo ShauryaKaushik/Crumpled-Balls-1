cc434f4c76ed8d80fde32659bd8e9e263f573435
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-02 19:58:36 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -19,7 +19,7 @@ class Box {
       translate(pos.x,pos.y)
       rotate(angle)
       rectMode(CENTER);
-      strokeWeight(4);
+      strokeWeight(5);
       fill("blue");
       rect(0,0, this.width, this.height);
       pop();
