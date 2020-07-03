b3d885851433c8ebb6a7ec2d19e053ca393552c5
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-03 14:04:45 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -1,7 +1,7 @@
 class PaperBall{
     constructor(x, y,radius) {
         var options = {
-            "isStatic" : true,
+            "isStatic" : false,
             'restitution':1.5,
             'friction':0.5,
             'density':1.5
